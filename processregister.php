39be2d5f9c0a3291077927fec8ec37a9f5542d9c
Haruna Gana <62021895+HarryGana@users.noreply.github.com>
2020-04-29 11:42:30 +0100

    session 1

--- processregister.php
+++ processregister.php
@@ -1,16 +1,16 @@
-<?php
+<?php session_start()
 //collecting the data
 
 $errorCount = 0; 
 //veryfing the data, validation
 
-$First_name =$_POST['first_name'] != "" ? $_POST['first_name'] : $errorCount++;
-$last_name =$_POST ['last_name'] != "" ? $_POST['last_name'] : $errorCount++;
-$email =$_POST ['email']!= "" ? $_POST['email'] : $errorCount++;
-$password =$_POST ['password']!= "" ? $_POST['password'] : $errorCount++;
-$gender =$_POST ['gender']!= "" ? $_POST['gender'] : $errorCount++;
-$designation =$_POST ['designation']!= "" ? $_POST['designation'] : $errorCount++;
-$department =$_POST ['department']!= "" ? $_POST['department'] : $errorCount++;
+$first_name = $_POST['first_name'] != "" ? $_POST['first_name'] : $errorCount++;
+$last_name = $_POST['last_name'] != "" ? $_POST['last_name'] : $errorCount++;
+$email = $_POST['email']!= "" ? $_POST['email'] : $errorCount++;
+$password = $_POST['password']!= "" ? $_POST['password'] : $errorCount++;
+$gender = $_POST['gender']!= "" ? $_POST['gender'] : $errorCount++;
+$designation = $_POST['designation']!= "" ? $_POST['designation'] : $errorCount++;
+$department = $_POST['department']!= "" ? $_POST['department'] : $errorCount++;
 
 if(errorCount > 0){
     //redirect back and display error
