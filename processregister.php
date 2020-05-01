9b0f0cf6e780e075067bcbea0f11a1360b02d2e2
Haruna Gana <62021895+HarryGana@users.noreply.github.com>
2020-05-01 09:49:53 +0100

    process

--- processregister.php
+++ processregister.php
@@ -13,7 +13,7 @@ $designation = $_POST['designation']!= "" ? $_POST['designation'] : $errorCount+
 $department = $_POST['department']!= "" ? $_POST['department'] : $errorCount++;
 
 if($errorCount > 0){
-    $_SESSION["error"] = "You have" , $errorCount , "in you form submission";
+    $_SESSION["error"] = "You have" . $errorCount . "in you form submission";
     header("Location: register.php? ");
 } 
 else { 
